65e68d6f43d4c3da09d38cfdf955fbb0feaf5cb7
lenny <liangnian1155@gmail.com>
2015-11-22 14:17:32 +0800

    Signed-off-by: lenny <liangnian1155@gmail.com>

---
+++ python/uglynumber2.py
@@ -0,0 +1,27 @@
+#find the nth uglynumber
+class Solution(object):
+    def nthUglyNumber(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        answer=1
+        count=1
+        while n!=0:
+        	while answer%2==0:
+        		answer=answer/2
+        	while answer%3==0:
+        		answer=answer/3
+        	while answer%5==0:
+        		answer=answer/5
+        	if answer==1:
+        		n=n-1
+        	if n==0:
+        		break
+        	count=count+1
+        	answer=count
+        return count
+        	
+
+test=Solution()
+print(test.nthUglyNumber(279))
